b18c643fb7718cba615fb5742c7418f90cb77049
Gerson Koji Utsunomiya <gersonkoji@ciandt.com>
2018-07-31 14:27:20 -0300

    [POC] Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { VgAPI } from 'videogular2/core';
+import * as Hls from 'hls.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  @ViewChild('hlsNative') hlsNative: ElementRef;
+
+  api: VgAPI;
+
+  ngOnInit() {
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource('https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8');
+      hls.attachMedia(this.hlsNative.nativeElement);
+      hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        this.hlsNative.nativeElement.play();
+      });
+    }
+  }
+
+  onPlayerReady(api: VgAPI) {
+    this.api = api;
+  }
+
+  skipTime() {
+    this.api.getDefaultMedia().currentTime = 10;
+  }
+
+  skipTimeNative() {
+    this.hlsNative.nativeElement.currentTime = 10;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { VgCoreModule } from 'videogular2/core';
+import { VgControlsModule } from 'videogular2/controls';
+import { VgOverlayPlayModule } from 'videogular2/overlay-play';
+import { VgBufferingModule } from 'videogular2/buffering';
+import { VgStreamingModule } from 'videogular2/streaming';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
+    VgStreamingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
